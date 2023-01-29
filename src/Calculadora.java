6895122d2df84226af549c7de415f9f46fd65bc3
LuizPaulo23 <luizpauloueg@gmail.com>
2023-01-29 17:08:14 -0300

    CHANGED: calculadora finalizada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,32 @@
+//Aula 09-10
 public class Calculadora {
-    public static void main(String[] args){
-        System.out.println("Olá Mundo");
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
 
-
+        if(args[0].equals("somar")){
+            sum(x, y);
+        } else if(args[0].equals("subtrair")){
+            sub(x, y);
+        } else if(args[0].equals("multiplicar")){
+            mult(x, y);
+        } else if(args[0].equals("divisao")){
+            div(x, y);
+        } else{
+            System.out.println("ERRO: operação matemática " +
+                               "não econtrada");
+        }
     }
-
+    static void sum(int x, int y){
+        System.out.println(x + y);
+    }
+    static void sub(int x, int y){
+        System.out.println(x - y);
+    }
+    static void mult(int x, int y){
+        System.out.println(x * y);
+    }
+    static void div(int x, int y) {
+        System.out.println(x / y);
+    }
 }
