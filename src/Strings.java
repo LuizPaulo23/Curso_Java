34f637c6b9c6e65a71ad604b5df48d10d7a490c5
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-02 20:53:30 -0300

    ADD: Manipulando Strings

---
+++ src/Strings.java
@@ -0,0 +1,22 @@
+// Aula 19
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String x = "Hello World";
+        System.out.println(x.length()); //tamanho
+        System.out.println(x + ", Luiz Paulo"); //concatenar
+        System.out.println(x.contains("Hello")); // contains
+        System.out.println(x.indexOf("Hello")); // index
+        System.out.println(x.lastIndexOf("World")); // last index
+        System.out.println(x.toUpperCase()); // caixa alta
+        System.out.println(x.toLowerCase()); // // baixa
+        System.out.println(x.trim()); // removendo espaços à direita
+        System.out.println(x.substring(9)); // substring
+        System.out.println(x.substring(9)); // substring
+        System.out.println(x.equals("x"));
+
+    }
+
+
+}
