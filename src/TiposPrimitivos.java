463261f58ef396da50b1f951302e877d54049d3e
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-01 22:37:30 -0300

    ADD: Utilizando Scanner

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+    // Tipos primitivos
+
+        boolean isDrive = true;
+        byte b = 'a';
+        char c = 'c';
+        String character = "c"; // não é primitivo
+        short s = -129; // igual o int (só com menos espaço)
+        int i = 12345; // grande
+        long l = 1234567891L; // int = (com mais espaço)
+        float f = 19.65f; // 32bit (menor que o double)
+        double d = 19.65; //64 bit espaço
+
+
+
+    }
+
+}
