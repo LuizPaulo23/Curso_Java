75cfa40d8a584b469992af03a71c7e4ff2b8da72
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-03 22:16:02 -0300

    CHANGED: ORGANIZANDO AS PASTAS

--- src/User.java
+++
@@ -1,8 +0,0 @@
-// Aula 21 - Orientação a Objeto
-public class User {
-    // class é um molde
-    // membros - propriedades e métodos (comportamento)
-    public String firstName;
-    public String lastName;
-
-}
