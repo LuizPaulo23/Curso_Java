a622bea7c940eb5b4aedd30aa96e865fe5679af9
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-03 22:10:38 -0300

    ADD: Organizando Pastas

---
+++ OPP.java
@@ -0,0 +1,10 @@
+public class OPP {
+    public static void main(String[] args) {
+        //Orientação a objetos
+
+
+
+    }
+
+
+}
---
+++ src/User.java
@@ -0,0 +1,8 @@
+// Aula 21 - Orientação a Objeto
+public class User {
+    // class é um molde
+    // membros - propriedades e métodos (comportamento)
+    public String firstName;
+    public String lastName;
+
+}
