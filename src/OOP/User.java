17b97196ac27d1957380955bd812ab3c7c3d8430
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-04 01:09:43 -0300

    CHANGED: Pkg e OOP

---
+++ src/OOP/User.java
@@ -0,0 +1,14 @@
+// Aula 21
+// Orientação a Objeto
+package OOP;
+
+public class User {
+    // Class é um molde
+    // Membro - propriedade e metodos (comportamento)
+    public String firstName;
+    public String lastName;
+
+
+
+
+}
---
+++ src/OOP/UserOPP.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class UserOPP {
+    public static void main(String[] args) {
+        User userA = new User();
+        userA.firstName = "Luiz Paulo";
+        userA.lastName = "Tavares Goncalves";
+
+        System.out.println(userA.firstName);
+        System.out.println(userA.lastName);
+    }
+
+}
