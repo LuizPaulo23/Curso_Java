16468aa47fd581e14c026ff8dcfc05acc66aa04f
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-04 18:21:05 -0300

    ADD: Comparando Arrays

---
+++ src/OOP/ComparadoArrays.java
@@ -0,0 +1,16 @@
+package OOP;
+
+import java.util.Arrays;
+
+public class ComparadoArrays {
+
+    public static void main(String[] args) {
+
+        int[] numbersA = new int[]{1,2,3};
+        int[] numbersB = new int[]{1,2,3};
+
+        System.out.println(Arrays.equals(numbersA, numbersB));
+
+    }
+
+}
