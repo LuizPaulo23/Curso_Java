25ad0268721bf6e9156d82693d464b0ca8b99c6f
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-04 22:20:08 -0300

    Add: Sobrecarga

---
+++ src/OOP/UserSobrecarga.java
@@ -0,0 +1,36 @@
+package OOP;
+
+public class UserSobrecarga {
+
+    private String firstName;
+    private String lastName;
+
+    public user(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+
+    }
+
+    public String output(){
+    return firstName.toUpperCase()+ " " + lastName.toUpperCase();
+
+    }
+
+}
